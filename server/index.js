7ade4ff13290fb3500e39a08f76a83b7a431215e
seungwonleee <seungwon.code@gmail.com>
2021-08-03 19:45:04 +0900

    Add: limit data size

--- server/index.js
+++ server/index.js
@@ -12,8 +12,8 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 const datas = [
   {
